025610e448602cc5050361e048b62c286741d751
Alecia Vogel <hello@alecia.ca>
2018-06-08 15:51:20 -0600

    Missed one

--- lib/measure_formtastic.rb
+++ lib/measure_formtastic.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
-require 'active_support'
-require 'active_support/core_ext'
+# require 'active_support'
+# require 'active_support/core_ext'
 
 module MeasureFormtastic
-  extend ActiveSupport::Autoload
+  # extend ActiveSupport::Autoload
 
-  autoload :Helpers, 'measure_formtastic/helpers'
-
-  eager_autoload do
-    autoload :FormBuilder, 'measure_formtastic/form_helper'
-  end
+  # autoload :Helpers, 'measure_formtastic/helpers'
+  #
+  # eager_autoload do
+  #   autoload :FormBuilder, 'measure_formtastic/form_helper'
+  # end
 end
--- lib/measure_formtastic/version.rb
+++ lib/measure_formtastic/version.rb
@@ -1,3 +1,3 @@
 module MeasureFormtastic
-  VERSION = '0.0.08'
+  VERSION = '0.0.09'
 end
